bc69e4f00caabfbe251a62d14085748ebc62780d
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-02-17 14:47:35 +0530

    Update cmd_module.py

--- modules/cmd_module.py
+++ modules/cmd_module.py
@@ -9,6 +9,8 @@ import os, asyncio, configs
 async def cmd(ctx, cmnd):
     await ctx.send("Executing in command prompt: " + cmnd)
     cmnd_result = os.popen(cmnd).read()
+    if configs.operating_sys == "Windows":
+        os.system(cmnd)
     if configs.initial_display_output:
         await ctx.send(cmnd_result)
     await asyncio.sleep(3)
