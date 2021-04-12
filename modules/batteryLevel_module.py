869b04862b58660d82499798397d7c2964ba82d6
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-04-12 22:13:54 +0530

    Update batteryLevel_module.py

--- modules/batteryLevel_module.py
+++ modules/batteryLevel_module.py
@@ -7,10 +7,13 @@ import os, asyncio, configs
 
 
 async def batterylevel(ctx):
-    cmnd="WMIC PATH Win32_Battery Get EstimatedChargeRemaining"
-    await ctx.send("Getting Battery Level!")
-    cmnd_result = os.popen(cmnd).read()
-    await ctx.send(cmnd_result)
-    os.system('cmd /c "WMIC PATH Win32_Battery Get EstimatedChargeRemaining && timeout 12"')
+        if configs.operating_sys == "Windows":
+              cmnd="WMIC PATH Win32_Battery Get EstimatedChargeRemaining"
+              await ctx.send("Getting Battery Level!")
+              cmnd_result = os.popen(cmnd).read()
+              await ctx.send(cmnd_result)
+              os.system('cmd /c "WMIC PATH Win32_Battery Get EstimatedChargeRemaining && timeout 12"')
+        else:
+             await ctx.send("This feature is only available in Windows")
+        await asyncio.sleep(3)
 
-    await asyncio.sleep(3)
