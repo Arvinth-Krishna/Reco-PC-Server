83fabd2523b1bed375061ae1f1e1db75b95ea502
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-04-13 13:14:55 +0530

    Adding SystemInfo module

---
+++ modules/systemInfo_module.py
@@ -0,0 +1,25 @@
+# Module: systemInfo
+# Description: Generates system Info
+# Usage: !systeminfo
+# Dependencies: time, os
+
+import os, time, asyncio, configs,discord
+
+
+async def systeminfo(ctx):
+        if configs.operating_sys == "Windows":
+             cmnd="systeminfo"
+             await ctx.send("Getting System Info.")
+             cmnd_result = os.popen(cmnd).read()
+
+             info = "SystemInfo.txt"
+             # popen() is similar to open()
+             file = open(info, 'w')
+             file.write(cmnd_result)
+             file.close()
+             await ctx.send(file=discord.File('SystemInfo.txt'))
+
+             os.system('cmd /c "systeminfo && echo. && echo. && echo Check your Discord Server to find the SystemInfo.txt file && echo.  && echo Close this terminal to resume Reco. && echo. &&  pause"')
+        else:
+             await ctx.send("This feature is only available in Windows")
+        await asyncio.sleep(3)
