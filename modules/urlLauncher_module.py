22532e021eaeb469a7ed6b0a40c24e9b47be536d
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-05-28 10:09:06 +0530

    Update urlLauncher_module.py

--- modules/urlLauncher_module.py
+++ modules/urlLauncher_module.py
@@ -9,6 +9,15 @@ import os, time, asyncio, configs
 async def url(ctx, txt):
     await ctx.send("Launching the website")
 
+    print(txt)
+    list=txt.split(" ")
+    
+    for i in range(len(list)):
+        if list[i].__contains__('https') or list[i].__contains__('www') :
+            txt=list[i]
+            print(txt)
+            break
+
     if configs.operating_sys == "Windows":
         os.system("start {0}".format(txt))
     elif configs.operating_sys == "Linux":
