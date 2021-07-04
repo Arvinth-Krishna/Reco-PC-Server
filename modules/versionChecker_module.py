e5646c32cd945231b9398c9af1c3b4ba3b975d85
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-07-04 06:25:10 +0530

    VersionChecker Module
    This module helps us to check the current version of Reco PC Server. So we can easily update Reco to the new version if it is available on Reco's GitHub.

---
+++ modules/versionChecker_module.py
@@ -0,0 +1,30 @@
+# Module: versionChecker
+# Description: Here you can check the current verion of Reco PC Server and it's improvements.
+# Usage: !version
+# Dependencies: time, os
+
+import os, asyncio, configs
+
+
+currentVersionNo=2.2
+
+print_log = '''Reco PC Server Version: {0}'''.format(currentVersionNo)
+
+
+
+async def version(ctx):
+     
+        if configs.operating_sys == "Windows":
+             
+             await ctx.send("Getting Current Version Info.")
+             info = "Reco_current_version_checker.txt"
+             
+             file = open(info, 'w')
+             file.write(print_log)
+             file.close()
+             await ctx.send(print_log)
+             os.system("start {0}".format(info))
+             
+        else:
+             await ctx.send(print_log)
+        await asyncio.sleep(3)
